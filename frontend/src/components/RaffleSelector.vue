60b4369d70893195f069a3ccc426b8e13543df0d
Astrian Zheng <happy.wheel6743@astrian.moe>
2023-05-09 21:16:32 +1000

    Navigate between different raffles

--- frontend/src/components/RaffleSelector.vue
+++ frontend/src/components/RaffleSelector.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, defineEmits, onMounted } from 'vue'
+
+const $props = defineProps(['current'])
+const $emit = defineEmits(['navigate'])
 
 const state = reactive({
   isDropdownActive: false,
+  raffleList: [] as RafflePool[],
+  raffleName: ''
 })
+
+onMounted(() => {
+  load()
+})
+
+const load = () => {
+  state.raffleList = JSON.parse(localStorage.getItem('raffle_list') || '[]')
+  if ($props.current === '_add') {
+    state.raffleName = '添加新抽奖池'
+  } else {
+    const raffle = state.raffleList.find(r => r.id === $props.current)
+    if (raffle) {
+      state.raffleName = raffle.name
+    }
+  }
+}
+
 </script>
 
 <template>
 <div class="dropdown" :class="state.isDropdownActive ? 'is-active' : ''">
   <div class="dropdown-trigger">
     <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" @click='() => { state.isDropdownActive = !state.isDropdownActive }'>
-      <span>添加新抽奖池</span>
+      <span>{{ state.raffleName }}</span>
       <span class="icon is-small">
         <font-awesome-icon :icon="['fas', 'angle-down']" />
       </span>
@@ -18,10 +40,17 @@ const state = reactive({
   </div>
   <div class="dropdown-menu" id="dropdown-menu" role="menu">
     <div class="dropdown-content">
-      <a href="#" class="dropdown-item">
+      <a href="#" class="dropdown-item" :class="$props.current === '_add' ? 'is-active' : ''" @click="() => {$emit('navigate', '_add'); state.isDropdownActive = false}">
         添加新抽奖池
       </a>
-      <hr class="dropdown-divider" />
+      <div v-if="state.raffleList.length > 0">
+        <hr class="dropdown-divider" />
+        <div v-for="(raffle, _) in state.raffleList" :key="raffle.id">
+          <a href="#" class="dropdown-item" :class="$props.current === raffle.id ? 'is-active' : ''" @click="() => {$emit('navigate', raffle.id); state.isDropdownActive = false}">
+            {{ raffle.name }}
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
--- frontend/src/pages/Admin.vue
+++ frontend/src/pages/Admin.vue
@@ -1,23 +1,39 @@
 <script setup lang="ts">
 import RaffleSelector from '../components/RaffleSelector.vue'
 import NewRaffle from '../components/NewRaffle.vue'
+import { onMounted, reactive } from 'vue'
+
+const state = reactive({
+  raffle_list: [],
+  current_raffle: '',
+})
 
 if (!localStorage.getItem('raffle_list')) {
   localStorage.setItem('raffle_list', JSON.stringify([]))
 }
 
-const newRaffle = () => {
-  console.log('newRaffle')
-  console.log(localStorage.getItem('current_raffle'))
+onMounted(() => {
+  load()
+})
+
+const load = () => {
+  const list = JSON.parse(localStorage.getItem('raffle_list') || '[]')
+  state.raffle_list = list
+  state.current_raffle = localStorage.getItem('current_raffle') ?? '_add'
+}
+
+const navigate = (id: string) => {
+  localStorage.setItem('current_raffle', id)
+  state.current_raffle = id
 }
 </script>
 
 <template>
 <div class="container">
   <h1 class="title">抽奖 bot</h1>
-  <RaffleSelector />
+  <RaffleSelector @navigate="navigate" :current="state.current_raffle" />
   <hr />
-  <NewRaffle @new-raffle="newRaffle" />
+  <NewRaffle @new-raffle="load" v-if="state.current_raffle === '_add'" />
 </div>
 </template>
 
